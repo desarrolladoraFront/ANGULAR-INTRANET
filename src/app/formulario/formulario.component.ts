da94348f2b2802a63cbd570f870212f90b978dd3
desarrolladoraFront <danielaandrade66@gmail.com>
2023-05-20 12:51:51 -0600

    Prueba con *ngIf=condition.

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
+
 export class FormularioComponent {
 
+  openAlert: boolean = false;
+    
+  mostrar_en_consola(name: string){
+    console.log(name);
+
+    this.openAlert = true;
+  }
 }
