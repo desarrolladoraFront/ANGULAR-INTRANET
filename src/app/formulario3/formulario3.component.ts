0fbd475a54a554c1b1fe720b159de8e79416a6b6
AnaDaniela96 <danielaandrade66@gmail.com>
2023-05-24 11:22:10 -0600

    Cambios en el botón submit. Sigue formando parte de formularios reactivos.

--- src/app/formulario3/formulario3.component.ts
+++ src/app/formulario3/formulario3.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario3',
   templateUrl: './formulario3.component.html',
   styleUrls: ['./formulario3.component.css']
 })
+
 export class Formulario3Component {
-  name = new FormControl('', Validators.required);
-  email = new FormControl('', [Validators.required, Validators.email]);
+
+  get name(){
+    return this.formUser.get('name') as FormControl;
+  }
+
+  get email(){
+    return this.formUser.get('email') as FormControl;
+  }
+
+  formUser= new FormGroup({
+    "name" : new FormControl('', Validators.required),
+    "email" : new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  procesar(){
+    console.log(this.formUser.value)
+  }
+
+  // name = new FormControl('', Validators.required);
+  // email = new FormControl('', [Validators.required, Validators.email]);
 
 }
