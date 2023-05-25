eebda6ac6f5418d121d06ce17915903ad31afc46
AnaDaniela96 <danielaandrade66@gmail.com>
2023-05-25 15:45:23 -0600

    Eliminé carpetas de prueba y agregué pluggins de MiAdminLTEv3.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { BotonesComponent } from './botones/botones.component';
-import { FormularioComponent } from './formulario/formulario.component';
-import { FooterComponent } from './footer/footer.component';
-import { AsideMenuComponent } from './aside-menu/aside-menu.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BuclesComponent } from './bucles/bucles.component';
-import { SwitchComponent } from './switch/switch.component';
-import { Formulario2Component } from './formulario2/formulario2.component';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Formulario3Component } from './formulario3/formulario3.component';
+
+
 import { DirectorioDeExtensionesComponent } from './directorio-de-extensiones/directorio-de-extensiones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    BotonesComponent,
-    FormularioComponent,
-    FooterComponent,
-    AsideMenuComponent,
-    BuclesComponent,
-    SwitchComponent,
-    Formulario2Component,
-    Formulario3Component,
     DirectorioDeExtensionesComponent
   ],
   imports: [
