abe48fd010bba40dcdcc85b0b54a056b1839563c
AnaDaniela96 <danielaandrade66@gmail.com>
2023-06-06 13:13:39 -0600

    Termina la vista de prototipo de cada lista de precios y agrega íconos para el menú.

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -8,18 +8,19 @@ import { DirectorioDeExtensionesComponent } from './directorio-de-extensiones/di
 
 //IMPORTACIÓN RUTA COMERCIAL
 import { ComercialComponent } from './comercial/comercial.component';
-import { CotizadorComponent } from './cotizador/cotizador.component';
-import { ListaDePreciosComponent } from './lista-de-precios/lista-de-precios.component';
-import { EntregasADomicilioComponent } from './entregas-a-domicilio/entregas-a-domicilio.component';
+import { CotizadorComponent } from './comercial/cotizador/cotizador.component';
+import { ListaDePreciosComponent } from './comercial/lista-de-precios/lista-de-precios.component';
+import { EntregasADomicilioComponent } from './comercial/entregas-a-domicilio/entregas-a-domicilio.component';
 import { ECommerceComponent } from './e-commerce/e-commerce.component';
-import { BoletinComponent } from './boletin/boletin.component';
-import { DistribuidorComponent } from './distribuidor/distribuidor.component';
-import { RefaccionPublicoComponent } from './refaccion-publico/refaccion-publico.component';
-import { BoletinDistribuidorComponent } from './boletin-distribuidor/boletin-distribuidor.component';
-import { BoletinFronterizoComponent } from './boletin-fronterizo/boletin-fronterizo.component';
-import { RefaccionDistribuidorComponent } from './refaccion-distribuidor/refaccion-distribuidor.component';
-import { FronterizoComponent } from './fronterizo/fronterizo.component';
-import { PublicoComponent } from './publico/publico.component';
+import { BoletinComponent } from './comercial/lista-de-precios/boletin/boletin.component';
+import { DistribuidorComponent } from './comercial/lista-de-precios/distribuidor/distribuidor.component';
+import { RefaccionPublicoComponent } from './comercial/lista-de-precios/refaccion-publico/refaccion-publico.component';
+import { BoletinDistribuidorComponent } from './comercial/lista-de-precios/boletin-distribuidor/boletin-distribuidor.component';
+import { BoletinFronterizoComponent } from './comercial/lista-de-precios/boletin-fronterizo/boletin-fronterizo.component';
+import { RefaccionDistribuidorComponent } from './comercial/lista-de-precios/refaccion-distribuidor/refaccion-distribuidor.component';
+import { FronterizoComponent } from './comercial/lista-de-precios/fronterizo/fronterizo.component';
+import { PublicoComponent } from './comercial/lista-de-precios/publico/publico.component';
+import { PerfilComponent } from './users/perfil/perfil.component';
 
 const routes: Routes = [
   {
@@ -43,13 +44,16 @@ const routes: Routes = [
         {path: 'listadeprecios/boletinfronterizo', component:BoletinFronterizoComponent },
         {path: 'listadeprecios/refacciondistribuidor', component: RefaccionDistribuidorComponent},
         {path: 'listadeprecios/fronterizo', component: FronterizoComponent},
-        
-
       {path: 'entregasadomicilio', component: EntregasADomicilioComponent},
       {path: 'e-commerce', component: ECommerceComponent},
       {path: '**', redirectTo: 'comercial'}
     ]
-  }
+  },
+  {path: 'users', component: UsersComponent,
+   children:[
+    {path: 'miperfil', component: PerfilComponent}
+   ]
+  }, 
 ]
 
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,18 +7,19 @@ import { PagesComponent } from './pages.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { ComercialComponent } from './comercial/comercial.component';
-import { CotizadorComponent } from './cotizador/cotizador.component';
-import { ListaDePreciosComponent } from './lista-de-precios/lista-de-precios.component';
-import { EntregasADomicilioComponent } from './entregas-a-domicilio/entregas-a-domicilio.component';
+import { CotizadorComponent } from './comercial/cotizador/cotizador.component';
+import { ListaDePreciosComponent } from './comercial/lista-de-precios/lista-de-precios.component';
+import { EntregasADomicilioComponent } from './comercial/entregas-a-domicilio/entregas-a-domicilio.component';
 import { ECommerceComponent } from './e-commerce/e-commerce.component';
-import { BoletinComponent } from './boletin/boletin.component';
-import { PublicoComponent } from './publico/publico.component';
-import { DistribuidorComponent } from './distribuidor/distribuidor.component';
-import { RefaccionPublicoComponent } from './refaccion-publico/refaccion-publico.component';
-import { BoletinDistribuidorComponent } from './boletin-distribuidor/boletin-distribuidor.component';
-import { BoletinFronterizoComponent } from './boletin-fronterizo/boletin-fronterizo.component';
-import { RefaccionDistribuidorComponent } from './refaccion-distribuidor/refaccion-distribuidor.component';
-import { FronterizoComponent } from './fronterizo/fronterizo.component';
+import { BoletinComponent } from './comercial/lista-de-precios/boletin/boletin.component';
+import { PublicoComponent } from './comercial/lista-de-precios/publico/publico.component';
+import { DistribuidorComponent } from './comercial/lista-de-precios/distribuidor/distribuidor.component';
+import { RefaccionPublicoComponent } from './comercial/lista-de-precios/refaccion-publico/refaccion-publico.component';
+import { BoletinDistribuidorComponent } from './comercial/lista-de-precios/boletin-distribuidor/boletin-distribuidor.component';
+import { BoletinFronterizoComponent } from './comercial/lista-de-precios/boletin-fronterizo/boletin-fronterizo.component';
+import { RefaccionDistribuidorComponent } from './comercial/lista-de-precios/refaccion-distribuidor/refaccion-distribuidor.component';
+import { FronterizoComponent } from './comercial/lista-de-precios/fronterizo/fronterizo.component';
+import { PerfilComponent } from './users/perfil/perfil.component';
 
 
 
@@ -40,7 +41,8 @@ import { FronterizoComponent } from './fronterizo/fronterizo.component';
     BoletinDistribuidorComponent,
     BoletinFronterizoComponent,
     RefaccionDistribuidorComponent,
-    FronterizoComponent
+    FronterizoComponent,
+    PerfilComponent
   ],
   imports: [
     CommonModule,
